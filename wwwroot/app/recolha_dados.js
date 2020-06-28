c29a95c8dba087df61c4681ad0a855cd362836c6
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2020-06-28 19:00:03 +0100

    new Chat

---
+++ wwwroot/app/recolha_dados.js
@@ -0,0 +1,43 @@
+var vetObj = [{id:0}];
+
+var btnSbt = $("#send");
+
+btnSbt.on('click', iniciar);
+
+$("#message").on('keypress', function (e){
+
+    if (e.keyCode == 13) {
+        iniciar();
+        return false;
+
+    }
+});
+
+function iniciar() {
+    var obj = { text: $("#message").val() };
+
+    addObj(obj);
+
+    $("#message").val("");
+}
+
+function addObj(obj) {
+
+    obj.date = new Date();
+    obj.id = vetObj[vetObj.length - 1].id + 1;
+    obj.source = true;
+
+    var div = $('<div/>');
+
+    div.addClass("col-md-12");
+
+    var p = $('<p/>').attr('class', 'admin-sms pull-right');
+
+    p.html(obj.text);
+
+    p.appendTo(div);
+
+    div.appendTo($('.chatMessages'));
+
+    vetObj.push(obj);
+}
