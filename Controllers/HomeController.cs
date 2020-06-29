ef308b86c9073768fbe33323bc120911fd81a7b0
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2020-06-29 20:52:24 +0100

    first select from database

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using ChatService.Application;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ChatService.Models;
@@ -12,14 +13,18 @@ namespace ChatService.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ChatContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController( ChatContext db)
         {
-            _logger = logger;
+            //_logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
+            var messages = _db.Message.Any();
+            //var data = from a in Message select a orderby a.LastName;
             return View();
         }
 
