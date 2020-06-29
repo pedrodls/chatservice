53730cbdda138251adc82eceee1aaf177878f4be
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2020-06-29 20:51:53 +0100

    database new Message model

--- Models/Message.cs
+++ Models/Message.cs
@@ -1,4 +1,12 @@
-$HEADER$namespace $NAMESPACE$
+using System;
+
+namespace ChatService.Models
 {
-  public class $CLASS$ {$END$}
+    public class Message
+    {
+        public int ID { get; set; }
+        public Boolean Source { get; set; }
+        public string Text { get; set; }
+        public DateTime date { get; set; }
+    }
 }
