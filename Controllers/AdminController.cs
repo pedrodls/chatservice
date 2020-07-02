45725aeac5439ea583d28c70ce0c187158e19379
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2020-07-02 17:15:02 +0100

    Controller fix

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,10 +1,21 @@
+using System.Collections.Generic;
+using System.Linq;
+using ChatService.Application;
+using ChatService.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ChatService.Controllers
 {
     public class AdminController : Controller
     {
+        private ChatContext _db;
+        
+        public IList<Message> Message { get; set; }
         // GET
+        public AdminController(ChatContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
             return View();
