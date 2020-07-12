7916483ad61a8bec677714eb6d411effc22d8411
PedroSousa2020 <sousapedro747@gmail.com>
2020-07-12 22:21:25 +0100

    Actualizando o token

---
+++ Application/CriarToken.cs
@@ -0,0 +1,25 @@
+﻿using Jose;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatService.Application
+{
+    public class CriarToken
+    {
+        public Dictionary<string, string> token = new Dictionary<string, string>();
+
+        public string CriandoToken(string nome, string exp)
+        {
+            token.Add(nome, exp);
+
+            var secretKey = UnicodeEncoding.UTF8.GetBytes("qwerty");
+
+            var cod = "";
+
+            cod = JWT.Encode(token, secretKey, JwsAlgorithm.HS256);
+
+            return cod;
+            
+        }
+    }
+}
